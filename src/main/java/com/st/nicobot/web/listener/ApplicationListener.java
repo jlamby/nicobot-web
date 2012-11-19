61fa691f8e628e92b079b61e76ebf685f4182d95
Julien LAMBY <lamby.julien@gmail.com>
2012-11-19 11:40:04 +0100

    Modification methode de demarrage du bot

--- src/main/java/com/st/nicobot/web/listener/ApplicationListener.java
+++ src/main/java/com/st/nicobot/web/listener/ApplicationListener.java
@@ -3,7 +3,7 @@ package com.st.nicobot.web.listener;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
-import com.st.nicobot.NicoBot;
+import com.st.nicobot.BotMain;
 
 
 public class ApplicationListener implements ServletContextListener {
@@ -16,13 +16,7 @@ public class ApplicationListener implements ServletContextListener {
 	@Override
 	public void contextInitialized(ServletContextEvent arg0) {
 		try {
-			NicoBot bot = new NicoBot("nicobot");
-			
-			//moar logs
-			bot.setVerbose(true);
-			
-			bot.connect("nl.quakenet.org");
-			bot.joinChannel("#zqsd");
+			BotMain.main(new String[] {});
 		}
 		catch(Exception ex) {
 			System.err.println(ex.getMessage());
