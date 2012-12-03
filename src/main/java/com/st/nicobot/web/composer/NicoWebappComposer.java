1c727ffd70b86209937a02ebe9177480380c47ee
Julien <lamby.julien@gmail.com>
2012-12-03 23:38:12 +0100

    Integration Pico container web

---
+++ src/main/java/com/st/nicobot/web/composer/NicoWebappComposer.java
@@ -0,0 +1,37 @@
+package com.st.nicobot.web.composer;
+
+import javax.servlet.ServletContext;
+
+import org.picocontainer.MutablePicoContainer;
+import org.picocontainer.web.WebappComposer;
+
+import com.st.nicobot.context.ComponentUtils;
+import com.st.nicobot.context.annotations.Component;
+import com.st.nicobot.utils.ClassLoader;
+
+
+/**
+ * @author jlamby
+ *
+ */
+public class NicoWebappComposer implements WebappComposer {
+	
+	@Override
+	public void composeApplication(MutablePicoContainer container, ServletContext servletContext) {
+		ComponentUtils.loadComponents(container, ClassLoader.getAllInstantiableClasses());
+		ComponentUtils.loadComponents(container, ClassLoader.getClassAnnotedWith(Component.class));
+
+		ComponentUtils.publishApplicationContext(container);
+	}
+
+	@Override
+	public void composeSession(MutablePicoContainer container) {
+		
+	}
+
+	@Override
+	public void composeRequest(MutablePicoContainer container) {
+		
+	}
+
+}
--- src/main/java/com/st/nicobot/web/listener/ApplicationListener.java
+++ src/main/java/com/st/nicobot/web/listener/ApplicationListener.java
@@ -1,12 +1,22 @@
 package com.st.nicobot.web.listener;
 
 import javax.servlet.ServletContextEvent;
-import javax.servlet.ServletContextListener;
 
-import com.st.nicobot.BotMain;
+import org.picocontainer.ComponentMonitor;
+import org.picocontainer.LifecycleStrategy;
+import org.picocontainer.gems.monitors.Slf4jComponentMonitor;
+import org.picocontainer.lifecycle.ReflectionLifecycleStrategy;
+import org.picocontainer.web.PicoServletContainerListener;
+import org.picocontainer.web.ScopedContainers;
+
+import com.st.nicobot.NicoBot;
+import com.st.nicobot.property.NicobotProperty;
+import com.st.nicobot.services.PropertiesService;
 
 
-public class ApplicationListener implements ServletContextListener {
+public class ApplicationListener extends PicoServletContainerListener {
+
+	private static final long serialVersionUID = 1L;
 
 	@Override
 	public void contextDestroyed(ServletContextEvent arg0) {
@@ -15,11 +25,28 @@ public class ApplicationListener implements ServletContextListener {
 
 	@Override
 	public void contextInitialized(ServletContextEvent arg0) {
+		super.contextInitialized(arg0);
+		
+		ScopedContainers scopedContainers = (ScopedContainers) arg0.getServletContext().getAttribute(ScopedContainers.class.getName());
+		
+		PropertiesService props = scopedContainers.getApplicationContainer().getComponent(PropertiesService.class);
+		NicoBot bot = scopedContainers.getApplicationContainer().getComponent(NicoBot.class);
+		
 		try {
-			BotMain.main(new String[] {});
+			bot.connect(props.get(NicobotProperty.BOT_SERVER));
+			bot.joinChannel(props.get(NicobotProperty.BOT_CHAN));
+		} catch (Exception ex) {
+			ex.printStackTrace();
 		}
-		catch(Exception ex) {
-			System.err.println(ex.getMessage());
-		}
+	}
+	
+	@Override
+	protected ComponentMonitor makeAppComponentMonitor() {
+		return new Slf4jComponentMonitor();
+	}
+	
+	@Override
+	protected LifecycleStrategy makeLifecycleStrategy() {
+		return new ReflectionLifecycleStrategy(makeAppComponentMonitor());
 	}
 }
